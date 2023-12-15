a3a92f4589e72f38887ce2a23f56c62e9b43c761
dylanherbst <dylan.herbstwork@gmail.com>
2023-12-15 18:59:59 +0800

    mobile completed

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         console.log(entry);
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+            observer.unobserve(entry.target);
         } else {
             entry.target.classList.remove('show');
         }
     });
-});
+}); { threshold: [0.1] };
 
 const hiddenElements = document.querySelectorAll('.hidden');
 
